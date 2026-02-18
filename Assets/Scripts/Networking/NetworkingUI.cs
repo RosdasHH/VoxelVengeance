05898de8f2af69f6a3a303bcb9b4913e518e8f6f
RosdasHH <164037153+RosdasHH@users.noreply.github.com>
2026-02-18 18:06:13 +0100

    Fix external connection issues

--- Assets/Scripts/Networking/NetworkingUI.cs
+++ Assets/Scripts/Networking/NetworkingUI.cs
@@ -1,12 +1,21 @@
 using Newtonsoft.Json.Bson;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class NetworkingUI : MonoBehaviour
 {
+    NetworkManager networkManager;
+    UnityTransport unityTransport;
+    private void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+        unityTransport = GetComponent<UnityTransport>();
+    }
     public void hostServer()
     {
-        GetComponent<NetworkManager>().StartServer();
+        unityTransport.SetConnectionData("127.0.0.1", (ushort)6767);
+        networkManager.StartServer();
     }
 
    public void connectClient()
