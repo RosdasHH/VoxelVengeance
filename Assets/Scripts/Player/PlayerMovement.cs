082e64136d6e9c279a153f31dfc9cebb880f40f3
MonsieurCiao <140845297+MonsieurCiao@users.noreply.github.com>
2026-02-20 17:46:34 +0100

    smth

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,17 +17,20 @@ public class PlayerMovement : NetworkBehaviour
     private float speed;
 
     [SerializeField]
+    private float accelerationSpeed;
+
+    [SerializeField]
     public float rotationSpeed;
     private float accumulatedRotation;
 
     public override void OnNetworkSpawn()
     {
+        rb = GetComponent<Rigidbody>();
         if (IsOwner)
         {
-            rb = GetComponent<Rigidbody>();
             Instantiate(camPrefab, camSpawn);
+            Cursor.lockState = CursorLockMode.Locked;
         }
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void MovePlayer(Vector2 movementInput, Vector2 lookInput)
@@ -38,10 +41,22 @@ public class PlayerMovement : NetworkBehaviour
         accumulatedRotation += rotationAmount;
         transform.rotation = Quaternion.Euler(0, accumulatedRotation, 0);
 
-        transform.Translate(
-            movementInput.x * speed * deltaTime,
-            0,
-            movementInput.y * speed * deltaTime
-        );
+        if (movementInput.magnitude >= 0.1f)
+        {
+            Vector3 targetVelocity =
+                new Vector3(movementInput.x, 0f, movementInput.y).normalized * speed;
+            Vector3 curVelocity = rb.linearVelocity;
+            curVelocity.y = 0f;
+
+            Vector3 velocityError = targetVelocity - curVelocity;
+            Vector3 acceleration = velocityError * accelerationSpeed * deltaTime;
+            rb.AddForce(acceleration, ForceMode.Acceleration);
+        }
+
+        // transform.Translate(
+        //     movementInput.x * speed * deltaTime,
+        //     0,
+        //     movementInput.y * speed * deltaTime
+        // );
     }
 }
