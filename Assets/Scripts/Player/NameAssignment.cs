30f0e47a4b5993ef3d3b25327b18c64801e93b40
MonsieurCiao <140845297+MonsieurCiao@users.noreply.github.com>
2026-02-21 15:39:44 +0100

    weapon script made a lil more abstract
    can now add multiple weapons [up to 3]

--- Assets/Scripts/Player/NameAssignment.cs
+++ Assets/Scripts/Player/NameAssignment.cs
@@ -7,23 +7,31 @@ public class NameAssignment : NetworkBehaviour
 {
     [SerializeField]
     private TextMeshPro playerNameText;
+
     [SerializeField]
     private TextMeshPro NameInput;
 
-    public NetworkVariable<FixedString64Bytes> playerName = new NetworkVariable<FixedString64Bytes>("Player", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    public NetworkVariable<FixedString64Bytes> playerName = new NetworkVariable<FixedString64Bytes>(
+        "Player",
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server
+    );
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         playerName.OnValueChanged += OnNameChanged;
         playerNameText.text = playerName.Value.ToString();
-        if (IsOwner) SetNameTagServerRpc("Player " + NetworkManager.LocalClientId.ToString());
+        if (IsOwner)
+            SetNameTagServerRpc("Player " + NetworkManager.LocalClientId.ToString());
     }
+
     [ServerRpc]
     private void SetNameTagServerRpc(FixedString64Bytes name)
     {
         playerName.Value = name;
     }
+
     private void OnNameChanged(FixedString64Bytes pre, FixedString64Bytes post)
     {
         playerNameText.text = post.ToString();
@@ -31,7 +39,8 @@ public class NameAssignment : NetworkBehaviour
 
     public void changeName(string value)
     {
-        if (!IsOwner) return;
+        if (!IsOwner)
+            return;
         SetNameTagServerRpc(value);
     }
 }
--- Assets/Scripts/Player/UserInput.cs
+++ Assets/Scripts/Player/UserInput.cs
@@ -8,6 +8,10 @@ public class UserInput : MonoBehaviour
     public static bool WasEscapePressed;
     public static bool WasEscapePauseMenuPressed;
     public static bool WasShootPressed;
+    //weapon slots
+    public static bool SlotPressed1;
+    public static bool SlotPressed2;
+    public static bool SlotPressed3;
 
     public static PlayerInput playerInput;
     
@@ -16,6 +20,10 @@ public class UserInput : MonoBehaviour
     private InputAction _lookAction;
     private InputAction _wasEscapePauseMenuPressed;
     private InputAction _shootAction;
+    //weapon slots
+    private InputAction _slotPressed1;
+    private InputAction _slotPressed2;
+    private InputAction _slotPressed3;
 
     private bool inputEnabled = true;
 
@@ -27,6 +35,10 @@ public class UserInput : MonoBehaviour
         _wasEscapePressed = playerInput.actions["Escape"];
         _wasEscapePauseMenuPressed = playerInput.actions["EscapePauseMenu"];
         _shootAction = playerInput.actions["Attack"];
+
+        _slotPressed1 = playerInput.actions["Slot1"];
+        _slotPressed2 = playerInput.actions["Slot2"];
+        _slotPressed3 = playerInput.actions["Slot3"];
     }
 
     void OnEnable()
@@ -53,6 +65,11 @@ public class UserInput : MonoBehaviour
             MoveInput = _moveAction.ReadValue<Vector2>();
             LookInput = _lookAction.ReadValue<Vector2>();
             WasShootPressed = _shootAction.WasPressedThisFrame();
+
+            //weapon slots
+            SlotPressed1 = _slotPressed1.WasPressedThisFrame();
+            SlotPressed2 = _slotPressed2.WasPressedThisFrame();
+            SlotPressed2 = _slotPressed3.WasPressedThisFrame();
         }
         WasEscapePressed = _wasEscapePressed.WasPressedThisFrame();
         WasEscapePauseMenuPressed = _wasEscapePauseMenuPressed.WasPressedThisFrame();
